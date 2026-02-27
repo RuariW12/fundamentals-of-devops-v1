2fc019d0e25f3cfc301245b67fec3372bbcc0feb
RuariW12 <ruariw16@gmail.com>
2026-02-26 21:58:38 -0500

    added reusable module apps, and moved the provider from modules here

--- ch2/tofu/live/sample-app/main.tf
+++ ch2/tofu/live/sample-app/main.tf
@@ -1,6 +1,22 @@
+provider "aws" {                                               
+  region = "us-east-2"
+}
+
+
 module "sample_app_1" {
     source = "../../modules/ec2-instance"
 
     name = "sample-app-tofu-1"
 }
 
+module "sample_app_2" {
+    source = "../../modules/ec2-instance"
+
+    name = "sample-app-tofu-2"
+}
+
+module "sample_app_3" {
+    source = "../../modules/ec2-instance"
+
+    name = "sample-app-tofu-3"
+}
