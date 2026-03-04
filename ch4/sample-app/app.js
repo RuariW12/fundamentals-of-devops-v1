78150d1c8fccf9b7242f5cfc1d07adfd5aadd751
RuariW12 <ruariw16@gmail.com>
2026-03-04 15:43:28 -0500

    Slightly modified app and dockerfile for express

--- ch4/sample-app/app.js
+++ ch4/sample-app/app.js
@@ -1,4 +1,4 @@
-const http = require('http')
+const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
@@ -9,4 +9,3 @@ const port = process.env.PORT || 8080;
 server.listen(port,() => {
   console.log(`Listening on port ${port}`);
 });
-
